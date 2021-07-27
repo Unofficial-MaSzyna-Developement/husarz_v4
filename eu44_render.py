224169210126ba512e8ba86994f6625de927fab2
Hirek <polomarket73@gmail.com>
2021-07-27 13:36:09 +0200

    Upload all files

---
+++ eu44_render.py
@@ -0,0 +1,369 @@
+﻿#encoding: UTF-8
+
+from PIL import ImageDraw, ImageFont, Image
+from random import random
+from datetime import datetime, timedelta, date
+from time import gmtime, strftime
+from sys import maxint
+import time
+
+def byteSumToBools(toconv):
+	toconv-=1
+	subract=128
+	booltable=[False,False,False,False,False,False,False,False]
+	for i in range(8):
+		if((int(toconv)/int(subract))>=1):
+			booltable[i]=True
+			toconv-=subract
+		subract/=2
+	return booltable
+
+
+# Settings
+loading_time = 15 # Dlugosc ladowania systemow EU44
+
+
+# definicje kolorow
+czarny = (0,0,0)
+czarny_gsmr = (25,25,25)
+czarny2_gsmr = (10,10,10)
+szary_gsmr = (200,200,200)
+czarny_diag = (0,0,0)
+bialy =(255,255,255)
+bialy_diag =(255,255,255)
+jszary =(220,220,220)
+niebieski =(0,0,255)
+niebieski_diag =(0,0,255)
+ed = (255,254,2)
+jasnoniebieski_diag = (133,128,255)
+zolty_diag = (255,254,2)
+fiolet = (255, 0, 255)
+ref_white = (255, 255, 0)
+ref_red = (255, 0, 0)
+
+class eu44_render(abstractscreenrenderer):
+
+
+
+	def __init__(self, lookup_path):
+		self.podklad = self.openimage(lookup_path + "int/screen")
+		lookup_path = lookup_path + "screen/"
+		self.ertms = Image.open(lookup_path + "ertms.png")
+		self.diag = Image.open(lookup_path + "diagnostyczny.png")
+		self.sredni_arial = ImageFont.truetype('./fonts/arialbd.ttf', 80)
+		self.maly_arial = ImageFont.truetype('./fonts/arialbd.ttf', 60)
+		self.bmaly_arial = ImageFont.truetype('./fonts/arialbd.ttf', 40)
+		self.gsmr1 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 22)
+		self.gsmr2 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 14)
+		self.gsmr3 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 18)
+		self.gsmr4 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 80)
+		self.maska = Image.open(lookup_path + "maska.png")
+		self.sifa = Image.open(lookup_path + "sifa.png").convert("RGBA")
+		self.shp = Image.open(lookup_path + "shp.png").convert("RGBA")
+		self.panto_nmozna = Image.open(lookup_path + "panto_nmozna.png")
+		self.panto_mozna = Image.open(lookup_path + "panto_mozna.png")
+		self.ws_initized = Image.open(lookup_path + "ws_initized.png")
+		self.ws_ninitized = Image.open(lookup_path + "ws_ninitized.png")
+		self.brak_jazdy = Image.open(lookup_path + "brak_jazdy.png")
+
+		self.engine1off = Image.open(lookup_path + "engine_1_off.png")
+		self.engine2off = Image.open(lookup_path + "engine_2_off.png")
+		self.engine3off = Image.open(lookup_path + "engine_3_off.png")
+		self.engine4off = Image.open(lookup_path + "engine_4_off.png")
+		self.engine1on = Image.open(lookup_path + "engine_1_on.png")
+		self.engine2on = Image.open(lookup_path + "engine_2_on.png")
+		self.engine3on = Image.open(lookup_path + "engine_3_on.png")
+		self.engine4on = Image.open(lookup_path + "engine_4_on.png")
+		self.loading = Image.open(lookup_path + "loading.png")
+
+		self.kilometry = (random()*300000)+5000
+		self.last_time_update = 0
+		self.dzis = datetime.now().timetuple().tm_yday
+		self.rok = datetime.now().year
+		self.last_hour = 10
+		self.temp = (random()*15) + 20
+		self.last_time_update = 0
+		self.aktyw = 0
+		
+	def _render(self, state):
+		obrazek = self.podklad.copy()
+		
+		if state['battery'] or state['converter']:
+			seconds = state['seconds']	
+			dt = 0
+
+			draw = ImageDraw.Draw(obrazek)
+
+			if seconds != self.last_time_update:
+				dt = seconds - self.last_time_update
+				if dt < 0:
+					dt+=60
+				self.last_time_update = seconds
+
+			self.aktyw += dt
+			if self.aktyw<loading_time:
+				war_prawy = False
+				war_tacho = False
+			else:
+				war_prawy = True
+				war_tacho = True
+
+			if (war_tacho == False):
+				# Loading screen
+				if state['battery'] or state['converter']:
+					obrazek.paste(self.loading, (136, 2157), self.diag) # ekran ERTMS
+					obrazek.paste(self.loading, (136, 101), self.diag) # ekran diagnostyczny
+
+					# Pasek ladowania
+					if self.aktyw >= 0:
+						draw.rectangle(((3, 1670), (109, 1851)), fill=niebieski)
+					if self.aktyw >= 0:
+						draw.rectangle(((3, 1670), (109, 1851)), fill=niebieski)
+
+
+			else:
+				#czas
+				if state['seconds'] != self.last_time_update:
+					dt = state['seconds'] - self.last_time_update
+					if dt < 0:
+						dt+=60
+					self.last_time_update = state['seconds']
+				if state['hours']<10:
+					godz = "0" + str(state['hours'])
+				else:
+					godz = str(state['hours'])
+				if state['minutes']<10:
+					min = "0" + str(state['minutes'])
+				else:
+					min = str(state['minutes'])
+				if state['seconds']<10:
+					sec = "0" +str(state['seconds'])
+				else:
+					sec = str(state['seconds'])
+				
+				
+				#data
+				if self.last_hour == 23 and state['hours'] == 0:
+					self.dzis = self.dzis+1 # wlasnie wybila polnoc
+				self.last_hour = state['hours']
+				data = datetime(self.rok, 1, 1) + timedelta(self.dzis - 1)
+				dzien = datetime.weekday(data)
+				datab = data.strftime("%d.%m.%y")
+				data = data.strftime("%d.%m.%Y")
+				DayL = ['Pn','Wt',u'Śr','Cz','Pt','So','Ni']
+				zmienna_kanalu = str(state['radio_channel'])
+
+				speed = state['velocity']
+
+				if speed > 240:
+					speed = 240
+
+				if state['battery'] or state['converter']:
+					obrazek.paste(self.ertms, (136, 2157), self.ertms) # podklad pod ekran srodek
+					obrazek.paste(self.diag, (136, 101), self.diag) # ekran diagnostyczny
+
+					#ERTMS prędkościomierz
+
+					rotate = speed * 300 / 240 + 30
+					rad =  radians(rotate)
+					srodek_tacho = (876, 2685)
+					point = (-10,0)
+					p1 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (-10,300)
+					p2 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (-2,325)
+					p3 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (-2,400)
+					p4 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (10,0)
+					p8 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (10,300)
+					p7 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (2,325)
+					p6 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					point = (2,400)
+					p5 = (point[0]*cos(rad)-point[1]*sin(rad) + srodek_tacho[0],point[0]*sin(rad)+point[1]*cos(rad) + srodek_tacho[1])
+					draw.polygon([p1,p2,p3,p4,p5,p6,p7,p8],fill=jszary)
+					self.print_center(draw, '%d' % speed, 876, 2685, self.sredni_arial, czarny) # Wyświetlanie cyferki
+					
+					# Tempomat
+					tempomat = state['new_speed'] * 5
+					if tempomat > 0:
+						rotate = tempomat * 300 / 240 + 30 - 180
+						rad =  radians(rotate)
+						srodek = (876, 2675)
+						point = (0,-418)
+						p1 = (srodek[0]+point[0]*cos(rad)-point[1]*sin(rad),srodek[1]+point[1]*cos(rad)+point[0]*sin(rad))
+						point = (-12,-430)
+						p2 = (srodek[0]+point[0]*cos(rad)-point[1]*sin(rad),srodek[1]+point[1]*cos(rad)+point[0]*sin(rad))
+						point = (0,-442)
+						p3 = (srodek[0]+point[0]*cos(rad)-point[1]*sin(rad),srodek[1]+point[1]*cos(rad)+point[0]*sin(rad))
+						point = (12,-430)
+						p4 = (srodek[0]+point[0]*cos(rad)-point[1]*sin(rad),srodek[1]+point[1]*cos(rad)+point[0]*sin(rad))
+						draw.polygon([p1,p2,p3,p4],fill=zolty_diag)
+
+
+					# Cisnienie w PG
+
+					pg_pressure = state['eimp_pn1_bp']
+					if (pg_pressure > 8):
+						pg_pressure = 8
+
+					height = 1366 - (1087 / 8 * pg_pressure)
+					if (pg_pressure > 4.70):
+						# x, y, x + szerokosc, y - wysokosc
+						draw.rectangle(((1805, 1366), (1805 + 77, height)), fill=niebieski)
+					else:
+						draw.rectangle(((1805, 1366), (1805 + 77, height)), fill=zolty_diag)
+
+					
+
+					# Napiecie w sieci
+					voltage = state['traction_voltage'] / 1000 - 2.0
+					print(voltage)
+					if (voltage > 2.5):
+						voltage = 2.5
+
+					if (voltage < 0.0):
+						voltage = 0.0
+
+					height = 1162 - (971 / 2.5 * voltage) # pozycja bezwzgledna lewego rogu Y, wysokosc / zakres * wartosc
+
+					draw.rectangle(((335, 1162), (335 + 36, height)), fill=niebieski) # X, Y, X + szer, wysokosc
+
+
+					# Cylindry hamulcowe
+
+					ch_pressure = float(state['eimp_pn1_bc'])
+					#self.print_center(draw, '%d' % ch_pressure, 876, 2685, self.sredni_arial, czarny) # Wyświetlanie cyferki
+					if (ch_pressure > 5):
+						ch_pressure = 5
+
+
+					draw.rectangle(((2150, 1088),(2150 + (452 / 5 * ch_pressure), 1088 + 74)), fill=zolty_diag)
+
+					draw.rectangle(((2150, 1288),(2150 + (452 / 5 * ch_pressure), 1288 + 74)), fill=zolty_diag)
+
+					#cisnienie w ZG
+
+					zg_pressure = state['eimp_pn1_sp']
+
+					if zg_pressure > 12:
+						zg_pressure = 12
+
+					height = 280 + 695 - (695 / 12 * zg_pressure) # pozycja bezwzgledna lewego rogu Y, wysokosc / zakres * wartosc
+
+					draw.rectangle(((2163, 280+695), (2163 + 74, height)), fill=zolty_diag) # X, Y, X + szer, wysokosc
+
+					
+					# SIŁY
+
+					force = state['eimp_c1_fr'] / 4
+					if (force > 90):
+						force = 90
+					if (force < -90):
+						force = -90
+					if (force > 0):
+						height = 1246 - (1055 / 90 * force) # pozycja bezwzgledna lewego rogu Y, wysokosc / zakres * wartosc
+						draw.rectangle(((914, 1246), (914 + 99, height)), fill=niebieski) # X, Y, X + szer, wysokosc
+						draw.rectangle(((1133, 1246), (1133 + 99, height)), fill=niebieski) # X, Y, X + szer, wysokosc
+						draw.rectangle(((1353, 1246), (1353 + 99, height)), fill=niebieski) # X, Y, X + szer, wysokosc
+						draw.rectangle(((1572, 1246), (1572 + 99, height)), fill=niebieski) # X, Y, X + szer, wysokosc
+
+					if (force < 0):
+						force = force * -1
+						height = 1246 - (1055 / 90 * force) # pozycja bezwzgledna lewego rogu Y, wysokosc / zakres * wartosc
+						draw.rectangle(((914, 1246), (914 + 99, height)), fill=zolty_diag) # X, Y, X + szer, wysokosc
+						draw.rectangle(((1133, 1246), (1133 + 99, height)), fill=zolty_diag) # X, Y, X + szer, wysokosc
+						draw.rectangle(((1353, 1246), (1353 + 99, height)), fill=zolty_diag) # X, Y, X + szer, wysokosc
+						draw.rectangle(((1572, 1246), (1572 + 99, height)), fill=zolty_diag) # X, Y, X + szer, wysokosc
+
+					# Ampery
+					current = state['eimp_c1_ihv']
+					if (current > 3000):
+						current = 3000
+					if (current < 0):
+						current = current * -1
+					height = 1244 - (float(float(1051) / float(3000)) * current) # pozycja bezwzgledna lewego rogu Y, wysokosc / zakres * wartosc
+					draw.rectangle(((624, 1244), (624 + 32, height)), fill=niebieski) # X, Y, X + szer, wysokosc
+
+					# Siła realizowana
+					frt = (state['eimp_c1_frt'])
+					if not -maxint-1 <= frt <= maxint:
+						frt = (frt + (maxint + 1)) % (2 * (maxint + 1)) - maxint - 1
+					end = int(frt / 2 - 90)
+					draw.pieslice((1528,2246,2375, 3026), -90, end, fill=niebieski)
+
+					#siła hamowania
+					frb = (state['eimp_c1_frb'])
+					if not -maxint-1 <= frb <= maxint:
+						frb = (frb + (maxint + 1)) % (2 * (maxint + 1)) - maxint - 1
+					end = int(-frb - 90)
+					draw.pieslice((1528,2246,2375, 3076), end, -90 , fill=ed)
+					obrazek.paste(self.maska,(1528,2246),self.maska)
+
+					# Procent siły
+					force_precetange = int(state['eimp_c1_pr'] * 100)
+					self.print_right(draw, '%d' % force_precetange, 2000, 2946, self.sredni_arial, czarny) # Wyświetlanie cyferki
+
+					# Kontrolki
+					# Czuwak/SIFA
+					if (state['ca']):
+						obrazek.paste(self.sifa, (2212, 3321), self.sifa)
+
+					# SHP
+					if (state['shp']):
+						obrazek.paste(self.shp, (333, 3318), self.shp)
+
+					# Moc kN diagnostyczy
+					force_full = state['eimp_t_fd']
+					if (force_full >= 0):
+						self.print_right(draw, '%d' % force_full, 2000, 1584, self.maly_arial, niebieski) # Wyświetlanie cyferki
+					else:
+						self.print_right(draw, '%d' % force_full * -1, 2000, 1584, self.maly_arial, zolty_diag) # Wyświetlanie cyferki
+
+					# Status WS
+					pantpressure = state['pantpress']
+					if pantpressure < 3.5 and state['traction_voltage'] < 500:
+						obrazek.paste(self.panto_nmozna, (616, 1536), self.panto_nmozna)
+					if pantpressure >= 3.5 and state['traction_voltage'] < 500:
+						obrazek.paste(self.panto_mozna, (616, 1536), self.panto_mozna)
+					if pantpressure >= 3.5 and state['traction_voltage'] > 2500 and state["main_init"] == True and state['linebreaker'] == False:
+						obrazek.paste(self.ws_ninitized, (616, 1536), self.ws_ninitized)
+					if pantpressure >= 3.5 and state['traction_voltage'] > 2500 and state["main_init"] == False and state['linebreaker'] == False:
+						obrazek.paste(self.ws_initized, (616, 1536), self.ws_initized)
+
+					# Brak jazdy
+					if (state['linebreaker'] == False or state['eimp_pn1_bc'] > 0.2 or state['brakes_1_spring_active'] == True):
+						obrazek.paste(self.brak_jazdy, (1079, 1536), self.brak_jazdy)
+						
+
+					# Diag time
+					draw.text((2330,345), godz +":"+ min +":"+ sec, fill=czarny, font=self.maly_arial) # Godzina
+					draw.text((2300,164), DayL[dzien] + " " + datab, fill=czarny, font=self.maly_arial) # Godzina
+
+					# Silniki status
+
+					if (state['linebreaker']):
+						obrazek.paste(self.engine1on, (817, 1279), self.engine1on)
+						obrazek.paste(self.engine2on, (1057, 1279), self.engine2on)
+						obrazek.paste(self.engine3on, (1296, 1279), self.engine3on)
+						obrazek.paste(self.engine4on, (1535, 1279), self.engine4on)
+					else:
+						obrazek.paste(self.engine1off, (817, 1279), self.engine1off)
+						obrazek.paste(self.engine2off, (1057, 1279), self.engine2off)
+						obrazek.paste(self.engine3off, (1296, 1279), self.engine3off)
+						obrazek.paste(self.engine4off, (1535, 1279), self.engine4off)
+
+					# Ref status
+					ref_a = bytes(state['lights_front'])
+					ref_b = bytes(state['lights_rear'])
+
+					# Tu kiedyś będzie kod reflektorow
+
+
+
+		else:
+			self.aktyw = 0
+
+		return obrazek
---
+++ radio_render.py
@@ -0,0 +1,174 @@
+﻿#encoding: UTF-8
+
+from PIL import ImageDraw, ImageFont, Image
+from random import random
+from datetime import datetime, timedelta, date
+from time import gmtime, strftime
+from sys import maxint
+
+
+# definicje kolorow
+czarny = (0,0,0)
+czarny_gsmr = (25,25,25)
+czarny2_gsmr = (10,10,10)
+szary_gsmr = (200,200,200)
+czarny_diag = (0,0,0)
+bialy =(255,255,255)
+bialy_diag =(255,255,255)
+jszary =(218,218,218)
+niebieski =(0,0,255)
+niebieski_diag =(0,0,255)
+ed = (157,145,95)
+jasnoniebieski_diag = (133,128,255)
+zolty_diag = (255,254,2)
+
+
+class radio_render(abstractscreenrenderer):
+
+
+
+	def __init__(self, lookup_path):
+		self.podklad = self.openimage(lookup_path + "ek1")
+		lookup_path = lookup_path + "screen/"
+		self.ertms = Image.open(lookup_path + "ertms.png")
+		
+		self.sredni_arial = ImageFont.truetype('./fonts/arialbd.ttf', 34)
+		self.maly_arial = ImageFont.truetype('./fonts/arialbd.ttf', 26)
+		self.bmaly_arial = ImageFont.truetype('./fonts/arialbd.ttf', 16)
+		self.gsmr1 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 22)
+		self.gsmr2 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 14)
+		self.gsmr3 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 18)
+		self.gsmr4 = ImageFont.truetype('./fonts/myriadpro-regular.otf', 80)
+		
+		self.kilometry = (random()*300000)+5000
+		self.last_time_update = 0
+		self.dzis = datetime.now().timetuple().tm_yday
+		self.rok = datetime.now().year
+		self.last_hour = 10
+		self.temp = (random()*15) + 20
+	def _render(self, state):
+
+		obrazek = self.podklad.copy()
+		draw = ImageDraw.Draw(obrazek)
+		#czas
+		if state['seconds'] != self.last_time_update:
+			dt = state['seconds'] - self.last_time_update
+			if dt < 0:
+				dt+=60
+			self.last_time_update = state['seconds']
+		if state['hours']<10:
+			godz = "0" + str(state['hours'])
+		else:
+			godz = str(state['hours'])
+		if state['minutes']<10:
+			min = "0" + str(state['minutes'])
+		else:
+			min = str(state['minutes'])
+		if state['seconds']<10:
+			sec = "0" +str(state['seconds'])
+		else:
+			sec = str(state['seconds'])
+#data
+		if self.last_hour == 23 and state['hours'] == 0:
+			self.dzis = self.dzis+1 # wlasnie wybila polnoc
+		self.last_hour = state['hours']
+		data = datetime(self.rok, 1, 1) + timedelta(self.dzis - 1)
+		dzien = datetime.weekday(data)
+		data = data.strftime("%d.%m.%Y")
+		DayL = ['Pn','Wt',u'Śr','Cz','Pt','So','Nd']
+		zmienna_kanalu = str(state['radio_channel'])
+		if state['battery']:
+	#GSMR------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+			#tlo
+			draw.rectangle(((1287,15),(1756,304)), fill=czarny2_gsmr)
+			draw.rectangle(((1290,25),(1751,298)), fill=szary_gsmr)
+			#ikonka trojkat rog
+			draw.polygon([1293,69,1325,69,1325,42],fill=czarny_gsmr)
+			
+			#ikonka jasnosc ekranu?
+			draw.polygon([1720,165,1745,165,1745,225],outline=czarny_gsmr)
+			draw.line((1725,176, 1745, 176), fill=czarny_gsmr, width=2)
+			draw.line((1730,187, 1745, 187), fill=czarny_gsmr, width=2)		
+			draw.line((1735,198, 1745, 198), fill=czarny_gsmr, width=2)	
+			draw.polygon([1740,210,1745,210,1745,225],fill=czarny_gsmr)
+			
+			#ikonka nad sloneczniekim
+			draw.ellipse([1294,98,1326,151],outline=czarny_gsmr)
+			draw.pieslice([1294,98,1326,151],-90, 90, fill=czarny_gsmr)
+			
+			#ikonka sloneczka
+			draw.ellipse([1298,170,1322,214],outline=czarny_gsmr)
+			draw.line((1310,164, 1310, 170), fill=czarny_gsmr, width=1)
+			draw.line((1324,167, 1320, 176), fill=czarny_gsmr, width=1)
+			draw.line((1327,192, 1322, 192), fill=czarny_gsmr, width=1)
+			draw.line((1324,215, 1319, 207), fill=czarny_gsmr, width=1)		
+			draw.line((1310,220, 1310, 214), fill=czarny_gsmr, width=1)	
+			draw.line((1297,215, 1302, 206), fill=czarny_gsmr, width=1)	
+			draw.line((1293,192, 1298, 192), fill=czarny_gsmr, width=1)	
+			draw.line((1296,168, 1302, 177), fill=czarny_gsmr, width=1)	
+			
+
+			#placek po srodku
+			draw.rectangle(((1482,76),(1571,197)), fill=czarny_gsmr)
+			draw.rectangle(((1488,69),(1563,75)), fill=czarny_gsmr)
+			draw.rectangle(((1488,190),(1563,204)), fill=czarny_gsmr)
+			draw.pieslice([1482,190,1499,204],90, 180, fill=czarny_gsmr)
+			draw.pieslice([1555,190,1571,204],0, 90, fill=czarny_gsmr)
+			draw.pieslice([1482,69,1499,83],180, 270, fill=czarny_gsmr)
+			draw.pieslice([1555,69,1571,83],270, 0, fill=czarny_gsmr)
+			
+			#ikonka glosnosci
+			draw.line((1675,195, 1675, 224), fill=czarny_gsmr, width=2)
+			draw.line((1670,215, 1675, 224), fill=czarny_gsmr, width=2)
+			draw.line((1670,215, 1666, 215), fill=czarny_gsmr, width=2)
+			draw.line((1666,204, 1666, 215), fill=czarny_gsmr, width=2)
+			draw.line((1666,204, 1670, 204), fill=czarny_gsmr, width=2)
+			draw.line((1675,195, 1670, 204), fill=czarny_gsmr, width=2)
+			
+			#linie
+			draw.line((1330,25, 1330, 299), fill=czarny_gsmr, width=1)
+			draw.line((1628,25, 1628, 299), fill=czarny_gsmr, width=1)
+			draw.line((1330,57, 1750, 57), fill=czarny_gsmr, width=1)
+			draw.line((1342,228, 1750, 228), fill=czarny_gsmr, width=1)		
+			draw.line((1291,91, 1330, 91), fill=czarny_gsmr, width=1)	
+			draw.line((1291,159, 1330, 159), fill=czarny_gsmr, width=1)	
+			draw.line((1291,227, 1330, 227), fill=czarny_gsmr, width=1)	
+			draw.line((1628,89, 1750, 89), fill=czarny_gsmr, width=1)	
+			draw.line((1628,123, 1750, 123), fill=czarny_gsmr, width=1)	
+			draw.line((1628,158, 1750, 158), fill=czarny_gsmr, width=1)	
+			draw.line((1342,298, 1342, 228), fill=czarny_gsmr, width=1)	
+			draw.line((1382,298, 1382, 228), fill=czarny_gsmr, width=1)			
+			draw.line((1422,298, 1422, 228), fill=czarny_gsmr, width=1)			
+			draw.line((1462,298, 1462, 228), fill=czarny_gsmr, width=1)			
+			draw.line((1504,298, 1504, 228), fill=czarny_gsmr, width=1)			
+			draw.line((1546,298, 1546, 228), fill=czarny_gsmr, width=1)	
+			draw.line((1586,298, 1586, 228), fill=czarny_gsmr, width=1)							
+			draw.line((1668,298, 1668, 228), fill=czarny_gsmr, width=1)			
+			draw.line((1710,298, 1710, 228), fill=czarny_gsmr, width=1)	
+			draw.line((1432,57, 1432, 25), fill=czarny_gsmr, width=1)		
+			
+			#napisy
+			if state['radio_channel'] < 10:
+					draw.text((1340,56), "K " + zmienna_kanalu, font=self.gsmr4, fill=czarny_gsmr)
+			if (state['radio_channel']==10):
+					draw.text((1340,56), "K" + zmienna_kanalu, font=self.gsmr4, fill=czarny_gsmr)		
+					
+			draw.text((1340,138), u'Gruppenruf 3', fill=czarny_gsmr, font=self.gsmr1)	
+			draw.text((1340,163), u'Zew 3', fill=czarny_gsmr, font=self.gsmr1)		
+			draw.text((1489,128), u'ZEW 3', fill=szary_gsmr, font=self.maly_arial)	
+			draw.text((1344,32), godz +":"+ min +":"+ sec, fill=czarny_gsmr, font=self.gsmr1)
+			draw.text((1456,32), u'analog     (PKP)', fill=czarny_gsmr, font=self.gsmr1)
+			draw.text((1291,257), u'KASUJ', fill=czarny_gsmr, font=self.gsmr2)
+			draw.text((1346,240), u'ZEW', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1356,270), u'1', fill=czarny_gsmr, font=self.gsmr1)
+			draw.text((1386,240), u'ZEW', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1396,270), u'2', fill=czarny_gsmr, font=self.gsmr1)
+			draw.text((1426,240), u'ZEW', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1436,270), u'3', fill=czarny_gsmr, font=self.gsmr1)
+			draw.text((1466,255), u'RStp', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1512,255), u'SQL', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1547,255), u'MON', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1587,255), u'Kanal', fill=czarny_gsmr, font=self.gsmr3)
+			draw.text((1632,251), u'SYS', fill=czarny_gsmr, font=self.gsmr1)
+			draw.text((1670,255), u'CZAS', fill=czarny_gsmr, font=self.gsmr3)		
+		return obrazek
